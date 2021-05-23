8286c37d31ddf5d8ea13a5fc29f2361be19e5410
lrbteste <84533512+lrbteste@users.noreply.github.com>
2021-05-23 12:52:44 +0100

    added

---
+++ pointclouds/BRAGA/cloud.js
@@ -0,0 +1,43 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 33389236,
+    "boundingBox": {
+        "lx": 547873.0478820801,
+        "ly": 4599910.1095581059,
+        "lz": 180.38126373291017,
+        "ux": 548261.0822143555,
+        "uy": 4600298.143890381,
+        "uz": 568.4155960083008
+    },
+    "tightBoundingBox": {
+        "lx": 547873.048,
+        "ly": 4599910.11,
+        "lz": 180.381,
+        "ux": 548250.335,
+        "uy": 4600298.144,
+        "uz": 223.106
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 3.360475778579712,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
